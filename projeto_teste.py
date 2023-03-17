8a5ed2ad2634205457ae95746e20413025b2716e
ingridbianca <ingridbianca10@gmail.com>
2023-03-16 22:14:16 -0300

    primeiro commit

---
+++ projeto_teste.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import xlsxwriter
+
+def create_file():
+    workbook = xlsxwriter.Workbook('teste.xlsx')
+    worksheet = workbook.add_worksheet()
+    header_format = workbook.add_format({'bold': True, 'text_wrap': True, 'fg_color': '#6a75cc', 'border': 1})
+    worksheet.write('A1', 'Hello!', header_format)
+    
+    workbook.close()
+
+
+if __name__ == '__main__':
+    create_file()
+    
+    
